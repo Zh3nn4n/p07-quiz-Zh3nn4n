896595591341e4c3e7ecea50134390c87b092149
18037689 <Zh3nn4n>
2019-05-31 16:26:45 +0800

    completed

--- app/src/main/java/sg/edu/rp/soi/p07quiz/MainActivity.java
+++ app/src/main/java/sg/edu/rp/soi/p07quiz/MainActivity.java
@@ -19,10 +19,14 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<String> aa;
     ArrayList <String> al;
 
+
+    int setting = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         spinner = findViewById(R.id.spinner);
         lv = findViewById(R.id.lv);
@@ -30,10 +34,48 @@ public class MainActivity extends AppCompatActivity {
         al = new ArrayList<>();
 
 
-
-
         aa = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, al );
+
+        aa.add("apple");
+        aa.add("ball");
+        aa.add("cat");
         lv.setAdapter(aa);
+
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(position){
+                    case 0:
+                        setting =0;
+                        break;
+                    case 1:
+                        setting=1;
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                    setting =0;
+            }
+        });
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(setting ==0){
+                    Log.v("sg.edu.rp.soi.p07quiz","Item clicked "+al.get(position));
+                }
+                else if(setting==1){
+                    Toast.makeText(MainActivity.this,"Item clicked "+al.get(position),Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+
 
 
 
